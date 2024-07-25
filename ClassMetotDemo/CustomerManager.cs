64851c79a3ec96781783ca4bd41b466fa913b63c
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2024-07-25 16:09:02 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+
+        public void CustomerAdd(Customer customer) 
+        {
+            Console.WriteLine("Id Eklendi" + customer.Id);
+            Console.WriteLine("İsim Eklendi" + customer.Ad);
+            Console.WriteLine("Soyad Eklendi" + customer.Soyad);
+        }
+     
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Id Silindi" + customer.Id);
+            Console.WriteLine("İsim Silindi" + customer.Ad);
+            Console.WriteLine("Soyad Silindi" + customer.Soyad);
+        }
+
+        public void List(Customer[] customers) {
+
+            foreach (Customer musteriler in customers)
+            {
+                Console.WriteLine(musteriler.Ad + "\n" + musteriler.Soyad + "\n" + musteriler.Id);
+                Console.WriteLine("********");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using ClassMetotDemo;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Customer musteri = new Customer();
+        musteri.Id = 1;
+        musteri.Ad = "Berkay";
+        musteri.Soyad = "Şahin";
+
+        Customer musteri1 = new Customer();
+        musteri1.Id = 2;
+        musteri1.Ad = "Mehmet";
+        musteri1.Soyad = "Emin";
+
+        CustomerManager manage = new CustomerManager();
+        manage.CustomerAdd(musteri);
+        Customer[] customers = new Customer[] { musteri, musteri1 };
+        manage.List(customers);
+        manage.Delete(musteri1);
+    }
+}
